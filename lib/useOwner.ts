4235838be819a0d6af407fcc5aca9c89add8b840
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-06-13 19:37:45 -0400

    created a custom hook for getting the owners of the current document

--- lib/useOwner.ts
+++ lib/useOwner.ts
@@ -18,12 +18,12 @@ function UseOwner() {
   useEffect(() => {
     if (usersInRoom?.docs && usersInRoom.docs.length > 0) {
       const owners = usersInRoom.docs.filter((doc) => {
-        doc.data().role === "owner";
+        return doc.data().role === "owner";
       });
 
       if (
         owners.some((owner) => {
-          owner.data().userId === user?.emailAddresses[0].toString();
+          return owner.data().userId === user?.emailAddresses[0].toString();
         })
       ) {
         setIsOwner(true);
