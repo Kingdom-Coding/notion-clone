5a059a8d981756340ac0ce65accc21e4ffe161ea
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-06-13 19:34:04 -0400

    created a DeleteDocument component that gets the id of the document from the url and deletes it

--- components/DeleteDocument.tsx
+++ components/DeleteDocument.tsx
@@ -2,31 +2,74 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { Button } from "./ui/button";
+import { usePathname, useRouter } from "next/navigation";
+import { deleteDocument } from "@/actions/actions";
+import { toast } from "sonner";
 
 function DeleteDocument() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const roomId = pathname.split("/").pop();
+
+    if (!roomId) return;
+
+    startTransition(async () => {
+      const { success } = await deleteDocument(roomId);
+
+      if (success) {
+        setIsOpen(false);
+        router.replace("/");
+        toast.success("room was successfully deleted");
+      } else {
+        toast.error("failed to deleted room!");
+      }
+    });
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <Button asChild variant="destructive">
-        <DialogTrigger>Open</DialogTrigger>
+        <DialogTrigger>Delete</DialogTrigger>
       </Button>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>
+            Are you sure you want to delete this document?
+          </DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            This will delete your document and remove all the users from having
+            access to it.
           </DialogDescription>
         </DialogHeader>
+        <DialogFooter className="sm:justify-end gap-2">
+          <Button
+            type="button"
+            variant="destructive"
+            onClick={handleDelete}
+            disabled={isPending}
+          >
+            {isPending ? "Deleting" : "Delete"}
+          </Button>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
