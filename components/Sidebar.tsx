61885fbcb9bd2aee0949dd0fe7c9b8734ab2cb5a
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-06-14 21:10:41 -0400

    fixed a small bug in the display of the shared documents for the user

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -97,18 +97,18 @@ function Sidebar() {
             ))}
           </>
         )}
-      </div>
 
-      {groupedData.editor.length > 0 && (
-        <>
-          <h2 className="text-gray-500 font-semibold text-sm">
-            Shared Documents
-          </h2>
-          {groupedData.editor.map((doc) => {
-            <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />;
-          })}
-        </>
-      )}
+        {groupedData.editor.length > 0 && (
+          <>
+            <h2 className="text-gray-500 font-semibold text-sm">
+              Shared Documents
+            </h2>
+            {groupedData.editor.map((doc) => (
+              <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
+            ))}
+          </>
+        )}
+      </div>
     </>
   );
 
